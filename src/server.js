df2d6c136591428fbe3186fc91c7c2f1c30d37a2
nuhgroh2004 <nuhgrohramadani@mail.ugm.ac.id>
2026-04-21 13:16:20 +0700

    chore: bind backend to 0.0.0.0

--- src/server.js
+++ src/server.js
@@ -4,6 +4,6 @@ const app = require("./app");
 
 const PORT = Number(process.env.PORT) || 8080;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
