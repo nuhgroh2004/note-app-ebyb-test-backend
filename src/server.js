69f8bfc7b4b7f7953a5ff64e248e67a6d4b96966
nuhgroh2004 <nuhgrohramadani@mail.ugm.ac.id>
2026-04-21 12:57:08 +0700

    chore: default backend server to port 8080

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const app = require("./app");
 
-const PORT = Number(process.env.PORT) || 3000;
+const PORT = Number(process.env.PORT) || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
